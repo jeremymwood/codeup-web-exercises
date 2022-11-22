8d23ed2f237d1729f3516533b0224459144257c3
jeremy wood <jeremywood@jeremys-MacBook-Pro.local>
2022-11-22 11:32:27 -0600

    11:32

--- js/external.js
+++ js/external.js
@@ -8,14 +8,17 @@
 //
 // alert(`Great, ${favColor} is my favorite color too!`)
 
+
+
 let littleMermaid = (prompt(`How many days did you rent The Little Mermaid?`));
 let brotherBear = (prompt(`How many days did you rent Brother Bear?`));
 let hercules = (prompt(`How many days did you rent Hercules?`));
 let pricePerDay = (prompt(`What is the cost per day to rent a movie?`));
-let totalCost = (littleMermaid + brotherBear +hercules) * pricePerDay;
+let newTotalCost = (parseInt(littleMermaid) + parseInt(brotherBear) + parseInt(hercules)) * parseInt(pricePerDay);
 
-alert(`Great, you rented The Little Mermaid for ${littleMermaid} days, Brother Bear for ${brotherBear} days and Hercules for ${hercules} days.  At a price per day of $ ${pricePerDay} per day, your total cost is $ ${totalCost}.`);
+alert(`Great, you rented The Little Mermaid for ${littleMermaid} days, Brother Bear for ${brotherBear} days and Hercules for ${hercules} days.  At a price per day of $ ${pricePerDay} per day, your total cost is $ ${newTotalCost}.`);
 
+// const movies = [`The Little Mermaid`,`Brother Bear`,'Hercules'];
 
-
-// console.log(`The total cost for movie rentals is $` +  ${totalCost} + `dollars.');
+//next, i want to control the variables from an array
+//then trim white space and convert to camelCase
