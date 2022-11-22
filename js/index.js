be8ba8fe62b506d092be57fb72761737d37eda17
jeremy wood <jeremywood@jeremys-MacBook-Pro.local>
2022-11-22 10:25:23 -0600

    10:25

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 "use strict"
 
-let age = prompt(`hey, how old are you?`);
+// let age = prompt(`hey, how old are you?`);
+//
+// alert(`you are ${age} years old`);
 
-alert(`you are ${age} years old`);
+function increment(someNumber) {
+    return someNumber + 1;
+    console.log('you will never see this.');
+    return someNumber + 2; // This will never run
+}
