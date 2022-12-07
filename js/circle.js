4fc2cc0841ded26019ac771502e2c97307e816d5
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-07 09:56:17 -0600

    221207_0956 in class review

--- js/circle.js
+++ js/circle.js
@@ -7,39 +7,35 @@
 
         getArea: function (radius) {
             // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            let area = Math.PI * radius ** 2;
-            if(typeof radius === `number`){
-                return area;
-            }
-            return false
+            //dont need a condition
+            const area = Math.PI * this.radius ** 2;
             // TODO: return the proper value
+            return area;
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            let area = this.getArea();
             if(doRounding === true){
-                return Math.round(this.getArea());
-            }  else{
-                return false
+                area = Math.round(area);
             }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log(`Area of a circle with radius: ${this.radius}, is:`);
+            console.log(`Area of a circle with radius: ${this.radius}, is: ${area}`);
         }
     };
 
-
+    console.log(circle.getArea());
     // log info about the circle
-    console.log("Raw circle information");
+    console.log("Raw area:");
     circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
+    console.log("Rounded area:");
     circle.logInfo(true);
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+    //radius 3 above, 5 below
+    circle.radius = 5
 
     // log info about the circle
     console.log("Raw circle information");
