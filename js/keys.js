988a83e7990e603447b730ee85ce72fac49ccecd
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-01-20 09:51:55 -0600

    0951

---
+++ js/keys.js
@@ -0,0 +1,2 @@
+const MAPBOX_API_KEY = "pk.eyJ1IjoibWFkbWFyY29zIiwiYSI6ImNsNWw0YWNobTBnY3AzanN4dzdpemhpeG0ifQ.OO7BegDPBkP3SV0CaLbnyQ";
+const OPENWEATHER_API_KEY = '59e1b99e68e6d9bdb350bf932c7ac068';
